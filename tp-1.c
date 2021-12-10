e0802d4538ecb464b445ffb05be67a604fd0b070
greeghost <hugo.boulier@ens-rennes.fr>
2021-12-10 15:24:16 +0100

    clean tp1, add gitignore

--- tp-1.c
+++ tp-1.c
@@ -8,76 +8,99 @@
 #include <errno.h>
 
 
-void main(int argc, char **argv) {
+// Récupération via pgrep du pid du processus de nom procname, et rangement à l'adresse pid
+void get_pid_safely(char* procname, char** pid) {
+  char cmd[strlen("pgrep ") + strlen(procname)];
+  sprintf(cmd, "pgrep %s", procname);
+
+  FILE *fh = popen(cmd, "r");
+  if (fh == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  size_t len;
+  ssize_t read;
+
+  read = getline(pid, &len, fh);
+  (*pid)[read - 1] = 0; // enlèvement du \n à la fin
+}
+
+
+// Attachement à un processus via ptrace(PTRACE_ATTACH, ...)
+long attach(int pid) {
+  long trace = ptrace(PTRACE_ATTACH, pid, 0, 0);
+  if (trace != 0) {
+    perror("Error: ptrace cont did not succeed:");
+    exit(EXIT_FAILURE);
+  }
+  return trace;
+}
+
+
+// Recuperation via nm de l'adresse d'une fonction
+uint get_fun_addr(char* pid, char* fun) {
+  char cmd[strlen("nm /proc/") + strlen(pid) + strlen("/exe")];
+  sprintf(cmd, "nm /proc/%s/exe", pid);
+
+  size_t len;
+  uint fun_addr;
+  char type;
+  char symbol[64];
+  char* buff = NULL;
+
+  FILE *fh = popen(cmd, "r");
+  if (fh == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  for (ssize_t read = getline(&buff, &len, fh); read != -1; read = getline(&buff, &len, fh)) {
+    buff[read - 1] = 0;
+    sscanf(buff, "%x %c %s", &fun_addr, &type, symbol);
+    if (strcmp(symbol, fun) == 0) {
+      break;
+    }
+  }
+  // on renvoit la dernière adresse trouvée si on n'a pas trouvé la fonction
+  return fun_addr;
+}
+
+
+void write_at_function(char* pid, uint fun_addr, unsigned char* text, int length) {
+  printf("1\n");
+  char addr[strlen("/proc/") + strlen(pid) + strlen("/mem") + 1];
+  sprintf(addr, "/proc/%s/mem", pid);
+  FILE *fh = fopen(addr, "r+");
+    if (!fh) {
+    perror("Error while opening proc/pid/mem");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("1\n");
+
+  fseek(fh, fun_addr, 0);
+  fwrite(text, 1, length, fh);
+
+  fclose(fh);
+}
+
+// fonction main: remplacer la première instruction d'une fonction d'un processus par un trap
+int main(int argc, char **argv) {
   if (argc <= 2) {
     printf("Error: Two argument expected (process name & function name)\n");
     exit(EXIT_FAILURE);
   }
 
   char* pid = NULL;
+  get_pid_safely(argv[1], &pid);
+  printf("%s\n", pid);
+  uint fun_addr = get_fun_addr(pid, argv[2]);
+  int pid_int = atoi(pid);
 
-  // Recuperation via pgrep du pid du processus à tracer
-  {
-    char cmd[strlen("pgrep " + strlen(argv[1]))];
-    sprintf(cmd, "pgrep %s", argv[1]);
-    FILE *fh = popen(cmd, "r");
-    size_t len;
-    ssize_t read;
+  attach(pid_int);
+  waitpid(pid_int, 0, 0);
 
-    if (fh == NULL) {
-      exit(EXIT_FAILURE);
-    }
+  unsigned char trap = 0xCC;
+  write_at_function(pid, fun_addr, &trap, 1);
 
-    read = getline(&pid, &len, fh);
-    pid[read - 1] = 0; // virer le \n à la fin
-  }
-
-
-  printf("pid: %d\n", atoi(pid));
-  long trace = ptrace(PTRACE_ATTACH, atoi(pid), 0, 0);
-  waitpid(atoi(pid), 0, 0);
-
-
-  char addr[strlen("/proc/") + strlen(pid) + strlen("/mem") + 1];
-  sprintf(addr, "/proc/%s/mem", pid);
-  FILE *fh = fopen(addr, "r+");
-    if (!fh) {
-    fprintf(stdout, "Error : %s\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-
-  char* fun = NULL;
-  uint fun_addr;
-
-  // Recuperation via nm de l'adresse de la fonction victime du trap
-  {
-    char cmd[strlen("nm /proc/" + strlen(pid) + strlen("/exe"))];
-    sprintf(cmd, "nm /proc/%s/exe", pid);
-    FILE *fh = popen(cmd, "r");
-    size_t len;
-
-    if (fh == NULL) {
-      exit(EXIT_FAILURE);
-    }
-
-    for (ssize_t read = getline(&fun, &len, fh); read != -1; read = getline(&fun, &len, fh)) {
-      fun[read - 1] = 0;
-      if (strcmp(fun+19, argv[2]) == 0) {
-        fun[17] = 0;
-        printf("addr: %s\n", fun);
-
-        sscanf(fun, "%x", &fun_addr);
-      }
-    }
-  }
-
-  // trapping
-  fseek(fh, fun_addr, 0);
-  char trap = 0xCC;
-  fwrite(&trap, 1, 1, fh);
-
-  fclose(fh);
-
+  free(pid);
   exit(EXIT_SUCCESS);
 }
